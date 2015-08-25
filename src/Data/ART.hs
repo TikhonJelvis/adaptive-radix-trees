fe94c58146fe5fb5226fa4a6972659eccc1d7d7c
Tikhon Jelvis <tikhon@jelv.is>
2015-08-25 12:36:22 -0700

    Wrote a simple lookup function.

--- src/Data/ART.hs
+++ src/Data/ART.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE MultiWayIf   #-}
+{-# LANGUAGE BangPatterns        #-}
+{-# LANGUAGE MonadComprehensions #-}
+{-# LANGUAGE MultiWayIf          #-}
 
 {-# OPTIONS_GHC -funbox-strict-fields #-}
 module Data.ART where
 
 import           Control.Applicative ((<$>))
+import           Control.Monad       (guard)
 
 import qualified Data.ByteString     as Byte
 import qualified Data.List           as List
@@ -13,8 +15,12 @@ import qualified Data.Vector         as V
 import qualified Data.Vector.Unboxed as U
 import           Data.Word           (Word8)
 
-type Key   = Byte.ByteString
-type Chunk = Word8
+import           Prelude             hiding (lookup)
+
+type Key    = Byte.ByteString
+type Chunk  = Word8
+
+type Prefix = Byte.ByteString
 
 (!~) :: Vector a -> Chunk -> a
 vector !~ key = vector ! (fromIntegral key)
@@ -37,6 +43,14 @@ data Node48 a = Node48 !(Vector (Maybe Chunk)) !(Vector (ART a)) deriving (Show,
 -- | Stores 49–256 children in a chunk-indexed array of 256 elements.
 newtype Node256 a = Node256 (Vector (Maybe (ART a))) deriving (Show, Eq)
 
+data ART a = Empty
+           | Leaf Key a
+           | N4   Int Prefix !(Node4   a)
+           | N16  Int Prefix !(Node16  a)
+           | N48  Int Prefix !(Node48  a)
+           | N256 Int Prefix !(Node256 a)
+             deriving (Show, Eq)
+
 get4 :: Node4 a -> Chunk -> Maybe (ART a)
 get4 (Node4 pairs) chunk = snd <$> V.find ((== chunk) . fst) pairs
 
@@ -57,15 +71,29 @@ get48 (Node48 keys children) chunk = (children !~) <$> (keys !~ chunk)
 get256 :: Node256 a -> Chunk -> Maybe (ART a)
 get256 (Node256 children) chunk = children !~ chunk
 
+-- | Does the given key start with the given prefix?
+checkPrefix :: Int -> Prefix -> Key -> Bool
+checkPrefix depth prefix key = prefix' == Byte.take (Byte.length prefix') key
+  where prefix' = Byte.take depth prefix
+
+
+lookup :: Key -> ART a -> Maybe a
+lookup key = go key 0
+  where go !_   depth Empty                 = Nothing
+        go !_   depth (Leaf k v)            = [v | key == k]
+        go !key depth (N4 _ prefix node4)   =
+          get4 node4 (Byte.head key) >>= withPrefix key depth prefix
+        go !key depth (N16 _ prefix node16)  =
+          get16 node16 (Byte.head key) >>= withPrefix key depth prefix
+        go !key depth (N48 _ prefix node48)  =
+          get48 node48 (Byte.head key) >>= withPrefix key depth prefix
+        go !key depth (N256 _ prefix node256)  =
+          get256 node256 (Byte.head key) >>= withPrefix key depth prefix
+
+        withPrefix key depth prefix tree =
+          guard (checkPrefix depth prefix key) >> go (Byte.tail key) (depth + 1) tree
+
    -- TODO: Organize tests!
 -- test_vec = map go [0..24] == map (flip List.elemIndex ls) [0..24]
 --   where go x = binarySearch x $ V.fromList ls
 --         ls = [11..13] ++ [15..17] ++ [20]
-
-data ART a = Empty
-           | Leaf Key a
-           | N4   !(Node4   a)
-           | N16  !(Node16  a)
-           | N48  !(Node48  a)
-           | N256 !(Node256 a)
-             deriving (Show, Eq)
