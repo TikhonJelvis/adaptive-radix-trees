4b63e04136c282be29bde939ca64709e2ee4778f
Tikhon Jelvis <tikhon@jelv.is>
2015-08-25 12:01:09 -0700

    Added functions for reading from the four node types.

--- src/Data/ART.hs
+++ src/Data/ART.hs
@@ -16,42 +16,51 @@ import           Data.Word           (Word8)
 type Key   = Byte.ByteString
 type Chunk = Word8
 
+(!~) :: Vector a -> Chunk -> a
+vector !~ key = vector ! (fromIntegral key)
+
              -- TODO: Figure out how to use unboxed vectors here!
 
 -- | Stores 1–4 children as an array of key/child pairs. This is
 -- handled efficiently under the hood because an unboxed vector of
 -- pairs is represented as a pair of unboxed vectors.
-newtype Node4 a = Node4 (Vector (Chunk, ART a))
+newtype Node4 a = Node4 (Vector (Chunk, ART a)) deriving (Show, Eq)
 
 -- | Stores 5–16 children as an array of key/child pairs. The relevant
 -- key can be found with a binary search.
-newtype Node16 a = Node16 (Vector (Chunk, ART a))
+newtype Node16 a = Node16 (Vector (Chunk, ART a)) deriving (Show, Eq)
 
 -- | Stores 17–48 children as a chunk-indexed 256-element array of
 -- keys into an array of 48 children.
-data Node48 a = Node48 !(Vector (Maybe Int)) !(Vector (ART a))
+data Node48 a = Node48 !(Vector (Maybe Chunk)) !(Vector (ART a)) deriving (Show, Eq)
 
 -- | Stores 49–256 children in a chunk-indexed array of 256 elements.
-newtype Node256 a = Node256 (Vector (Maybe (ART a)))
+newtype Node256 a = Node256 (Vector (Maybe (ART a))) deriving (Show, Eq)
 
-get4 :: Eq a => Node4 a -> Chunk -> Maybe (ART a)
+get4 :: Node4 a -> Chunk -> Maybe (ART a)
 get4 (Node4 pairs) chunk = snd <$> V.find ((== chunk) . fst) pairs
 
-binarySearch :: Chunk -> Vector Chunk -> Maybe Int
-binarySearch goal vector = go 0 (V.length vector)
-  where go !from !to = let i = from + (to - from) `div` 2 in
+get16 :: Node16 a -> Chunk -> Maybe (ART a)
+get16 (Node16 pairs) chunk = go 0 (V.length pairs)
+  where go !from !to =
+          let i      = from + (to - from) `div` 2
+              (k, v) = pairs ! i
+          in
           if | i + 1 == from || i == to -> Nothing
-             | vector ! i == goal     -> Just i
-             | vector ! i < goal     -> go (i + 1) to
+             | k == chunk             -> Just v
+             | k < chunk             -> go (i + 1) to
              | otherwise             -> go from i
 
-   -- TODO: Organize tests!
-test_vec = map go [0..24] == map (flip List.elemIndex ls) [0..24]
-  where go x = binarySearch x $ V.fromList ls
-        ls = [11..13] ++ [15..17] ++ [20]
+get48 :: Node48 a -> Chunk -> Maybe (ART a)
+get48 (Node48 keys children) chunk = (children !~) <$> (keys !~ chunk)
 
-get16 :: Eq a => Node16 a -> Chunk -> Maybe (ART a)
-get16 (Node16 pairs) chunk = undefined
+get256 :: Node256 a -> Chunk -> Maybe (ART a)
+get256 (Node256 children) chunk = children !~ chunk
+
+   -- TODO: Organize tests!
+-- test_vec = map go [0..24] == map (flip List.elemIndex ls) [0..24]
+--   where go x = binarySearch x $ V.fromList ls
+--         ls = [11..13] ++ [15..17] ++ [20]
 
 data ART a = Empty
            | Leaf Key a
@@ -59,3 +68,4 @@ data ART a = Empty
            | N16  !(Node16  a)
            | N48  !(Node48  a)
            | N256 !(Node256 a)
+             deriving (Show, Eq)
