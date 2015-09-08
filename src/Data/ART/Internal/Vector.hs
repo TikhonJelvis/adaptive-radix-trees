b273e404f18bb7d54fc0afdd5f4594531084e2c4
Tikhon Jelvis <tikhon@jelv.is>
2015-09-08 14:46:58 -0700

    Changed vector utility functions to work with vectors of pairs.

--- src/Data/ART/Internal/Vector.hs
+++ src/Data/ART/Internal/Vector.hs
@@ -12,19 +12,19 @@ import qualified Data.Vector.Generic.Mutable as MV
 -- | Given a sorted vector, returns an index containing the given element, if
 -- any. With multiple equal elements, the index returned is
 -- unspecified.
-binarySearch :: Ord a => a -> Vector a -> Maybe Int
+binarySearch :: Ord a => a -> Vector (a, b) -> Maybe (a, b)
 binarySearch needle haystack = go 0 (V.length haystack)
   where go !from !to =
-          let i = from + (to - from) `div` 2
-              k = haystack ! i
+          let i      = from + (to - from) `div` 2
+              (k, v) = haystack ! i
           in
           if | i + 1 == from || i == to -> Nothing
-             | k == needle            -> Just i
+             | k == needle            -> Just (k, v)
              | k < needle            -> go (i + 1) to
              | otherwise             -> go from i
 {-# INLINE binarySearch #-}
 
   -- TODO: Optimize?
-insert :: Ord a => a -> Vector a -> Vector a
-insert x vector = V.concat [before, V.singleton x, after]
-  where (before, after) = V.span (< x) vector
+insert :: Ord a => (a, b) -> Vector (a, b) -> Vector (a, b)
+insert (k, v) vector = V.concat [before, V.singleton (k, v), after]
+  where (before, after) = V.span (\ (k', _) -> k' < k) vector
