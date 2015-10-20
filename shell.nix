5b1cd99f5d0c2f04c2a0381334658e506bb7aa4c
Tikhon Jelvis <tikhon@jelv.is>
2015-10-19 20:35:54 -0700

    Started using Nixos to develop this package. Whoo.

---
+++ shell.nix
@@ -0,0 +1,33 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, bytestring, deepseq, HUnit, primitive
+      , QuickCheck, stdenv, tasty, tasty-quickcheck, vector
+      , vector-algorithms
+      }:
+      mkDerivation {
+        pname = "adaptive-radix-trees";
+        version = "0.1.0.0";
+        src = ./.;
+        libraryHaskellDepends = [
+          base bytestring deepseq primitive vector vector-algorithms
+        ];
+        testHaskellDepends = [
+          base HUnit QuickCheck tasty tasty-quickcheck
+        ];
+        description = "Efficient integer maps based on adaptive radix trees";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
