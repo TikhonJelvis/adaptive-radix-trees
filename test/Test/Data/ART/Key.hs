79fff7266b64ee08df3094047331e249582f9755
Tikhon Jelvis <tikhon@jelv.is>
2015-10-20 17:06:39 -0700

    Changed the test to ensure keys were the same size. (4 bytes for simplicity.)

--- test/Test/Data/ART/Key.hs
+++ test/Test/Data/ART/Key.hs
@@ -1,6 +1,8 @@
 module Test.Data.ART.Key where
 
-import qualified Data.ByteString as Byte
+import           Control.Monad         (replicateM)
+
+import qualified Data.ByteString       as Byte
 
 import           Test.QuickCheck
 import           Test.Tasty
@@ -13,10 +15,11 @@ tests = [ QC.testProperty "prefixes equal" prop_prefixEqual
         , QC.testProperty "prefixes empty" prop_prefixEmpty
         , QC.testProperty "prefixes" prop_prefix
         ]
-        
+
 newtype Pack = Pack { unpack :: Byte.ByteString }
 
-instance QC.Arbitrary Pack where arbitrary = Pack . Byte.pack <$> QC.listOf1 arbitrary
+instance QC.Arbitrary Pack where
+  arbitrary = Pack . Byte.pack <$> replicateM 4 arbitrary
 
 instance Show Pack where show (Pack bs) = show bs
 
@@ -24,8 +27,8 @@ prop_prefixEqual (Pack bs) = sharedPrefix bs bs == bs
 
 prop_prefixEmpty (Pack bs) = sharedPrefix bs Byte.empty == Byte.empty
 
-prop_prefix (Pack prefix) (Pack suffix₁) (Pack suffix₂) = start == prefix
-  where start = Byte.take (Byte.length prefix) $ sharedPrefix s₁ s₂
+prop_prefix (Pack prefix) (Pack suffix_1) (Pack suffix_2) = start == prefix
+  where start = Byte.take (Byte.length prefix) $ sharedPrefix s_1 s_2
 
-        s₁ = Byte.append prefix suffix₁
-        s₂ = Byte.append prefix suffix₂
+        s_1 = Byte.append prefix suffix_1
+        s_2 = Byte.append prefix suffix_2
